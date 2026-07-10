81c27c228083f1157c20c53d7ad6bdfa5ae9bdc0
Aman <aman.hasan@prilient.com>
2026-07-10 15:16:56 +0530

    add / in config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Graphic-designer-portfolio",
+  assetPrefix: "/Graphic-designer-portfolio/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;

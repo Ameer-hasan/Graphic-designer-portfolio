be3f166b254ced78507d0a6cac9c8c0a7e47dc38
Aman <aman.hasan@prilient.com>
2026-07-15 15:12:31 +0530

    changes photo and pdf path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
               }}
             >
               <img
-                src={"/person-img.jpeg"}
+                src={"/images/person-img.jpeg"}
                 alt={personalInfo.name}
                 style={{ width: "100%", height: "100%", objectFit: "fill" }}
               />
@@ -154,7 +154,7 @@ export default function Home() {
                 justifyContent: "center",
                 transition: "var(--transition-smooth)",
               }}
-              onClick={() => window.open("/PortFOLIO.pdf", "_blank")}
+              onClick={() => window.open("/images/PortFOLIO.pdf", "_blank")}
             >
               View Portfolio
             </a>
